62ca2b7f747c3e06db8562e3cedffd1a28155328
Jeremy Fox <jeremyfox36@gmail.com>
2020-06-16 13:08:19 +0100

    Adds library to calculate L-Moments

---
+++ FEHServicesLib/LMoments.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace FEHServicesLib
+{
+    public class LMoments
+    {
+        public float Beta0(float[] amaxArray)
+        {
+            float b0 = amaxArray.Sum() / amaxArray.Length;
+            float roundedB0 = (float)Math.Round(b0 * 100f)/100f;
+            return roundedB0;
+        }
+
+        public float Beta1(float[] amaxArray)
+        {
+            Array.Sort(amaxArray);
+
+            int n = amaxArray.Length;
+            
+            float b1_1 = 1f/(n * (n-1));
+
+            float b1_2 = 0f;
+            int i = 0;
+            foreach (float amax in amaxArray)
+            {
+                float intermediate = i * amax;
+                b1_2 += intermediate;
+                i++;
+            }
+
+            double output = b1_1 * b1_2;
+            //rounds to 2 decimal places
+            return (float)Math.Round(output * 100f)/100f;
+        }
+
+        public float Beta2(float[] amaxArray)
+        {
+            Array.Sort(amaxArray);
+
+            int n = amaxArray.Length;
+
+            float b2_1 = 1f / (n * (n - 1) * (n - 2));
+
+            float b2_2 = 0f;
+            int i = 0;
+            foreach(float amax in amaxArray)
+            {
+                float intermediate = i * (i-1) * amax;
+                b2_2 += intermediate;
+                i++;
+            }
+
+            double output = b2_1 * b2_2;
+            return (float)Math.Round(output * 100f) / 100f;
+        }
+
+        public float Lambda1(float[] amaxArray)
+        {
+            return Beta0(amaxArray);
+        }
+
+        public float Lambda2(float[] amaxArray)
+        {
+            return 2 * Beta1(amaxArray) - Beta0(amaxArray); 
+        }
+
+        
+    }
+}
---
+++ FEHServicesLibTests/LMomentsTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FEHServicesLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace FEHServicesLib.Tests
+{
+    [TestClass()]
+    public class LMomentsTests
+    {
+        float[] amaxData = {410,1150,899,420,3100,2530,758,1220,1330,1410,3100,2470,929,586,450,1040,1470,1070,2050,1430,3070,2360,1050,1900,1130,674,683,1500,2600,3480,1430,809,1010,1510,1650,1880,1470,1920,2530,1490
+};
+        //probability weighted moments
+        float beta0 = 1549.20f;
+        float beta1 = 1003.89f;
+        float beta2 = 759.02f;
+        //L-moments, rounded to integers
+        int Lambda1 = 1549;
+        int lambda2 = 458;
+        int lambda3 = 80;
+
+        [TestMethod()]
+        public void Beta0Test()
+        {
+            LMoments lMoments = new LMoments();
+
+            Assert.AreEqual(beta0, lMoments.Beta0(amaxData));
+        }
+
+        [TestMethod()]
+        public void Beta1Test()
+        {
+            LMoments lMoments = new LMoments();
+            float answer = lMoments.Beta1(amaxData);
+            Debug.WriteLine(answer);
+            Assert.AreEqual(beta1, answer);
+        }
+
+        [TestMethod()]
+        public void Beta2Test()
+        {
+            LMoments lMoments = new LMoments();
+            float answer = lMoments.Beta2(amaxData);
+            Debug.WriteLine(answer);
+            Assert.AreEqual(beta2, answer);
+        }
+
+        [TestMethod()]
+        public void Lambda2Test()
+        {
+            LMoments lMoments = new LMoments();
+            float answer = lMoments.Lambda2(amaxData);
+            Debug.WriteLine(answer);
+            Assert.AreEqual(lambda2, (int)answer);
+        }
+    }
+}
